8c65ca0c24f33f7c713850b1bd30702134888c22
Aleksandr Sosedskyi <sanya1sosedskiy@gmail.com>
2024-06-09 20:42:20 +0300

    Add .srt support for creating scenes

--- app/helpers/create_scenes.py
+++ app/helpers/create_scenes.py
@@ -1,50 +1,128 @@
+import datetime
 import json
-import pathlib
 import subprocess
 from itertools import batched
+from pathlib import Path
 
 import click
+import srt
+from pydantic import BaseModel
 
-from app.api.phrases.utils import parse_duration
+
+class Phrase(BaseModel):
+    id: str | None = None
+    start: datetime.timedelta
+    end: datetime.timedelta
+    text: str
+
+    @property
+    def output_name(self) -> str:
+        if self.id:
+            return f"{self.id}"
+
+        return f"{self.start}-{self.end}"
+
+
+def srt_parser(file_path: Path) -> list[Phrase]:
+    with open(file_path) as f:
+        content = f.read()
+
+        return [
+            Phrase(
+                start=phrase.start,
+                end=phrase.end,
+                text=phrase.content,
+            )
+            for phrase in srt.parse(content)
+        ]
+
+
+def json_parser(file_path: Path) -> list[Phrase]:
+    with open(file_path) as f:
+        content = f.read()
+        json_content = json.loads(content)
+        return [
+            Phrase(
+                id=phrase["id"],
+                start=phrase["start_in_movie"],
+                end=phrase["end_in_movie"],
+                text=phrase["full_text"],
+            )
+            for phrase in json_content
+        ]
+
+
+PHRASES_FILE_PARSERS_MAPPING = {
+    ".srt": srt_parser,
+    ".json": json_parser,
+}
+
+
+def handle_create_subtitles(phrases: list[Phrase], output_dir: Path) -> None:
+    for phrase in phrases:
+        subtitle = srt.Subtitle(
+            index=1,
+            start=datetime.timedelta(),
+            end=phrase.end,
+            content=phrase.text,
+        )
+
+        output_path = output_dir.joinpath(f"{phrase.output_name}.srt")
+
+        with open(output_path, "w") as f:
+            f.write(srt.compose([subtitle]))
 
 
 @click.command()
-@click.option("--movie-path", "-mp", required=True, type=click.Path(exists=True, dir_okay=False))
-@click.option("--phrases-path", "-pp", required=True, type=click.Path(exists=True, dir_okay=False))
+@click.option("--movie-path", "-m", required=True, type=click.Path(exists=True, dir_okay=False, path_type=Path))
+@click.option("--phrases-path", "-p", required=True, type=click.Path(exists=True, dir_okay=False, path_type=Path))
 @click.option(
     "--output-dir",
-    "-od",
+    "-o",
     required=True,
-    type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=pathlib.Path),
+    type=click.Path(exists=True, dir_okay=True, file_okay=False, path_type=Path),
 )
-@click.option("--workers", "-w", default=10, type=click.IntRange(1, 128))
-@click.option("--volume", "-vol", default=1, type=float)
-def create_scenes(movie_path: str, phrases_path: str, output_dir: str, volume: float, workers: int) -> None:
-    movie_extension = movie_path.split(".")[-1]
+@click.option("--workers", "-w", default=10, type=click.IntRange(min=1))
+@click.option("--volume", "-v", default=1.5, type=click.FloatRange(min=0.0))
+@click.option("--create-subtitles", "-s", default=False, is_flag=True)
+@click.option("--limit", "-l", default=99999, type=click.IntRange(min=1))
+def create_scenes(
+    movie_path: Path,
+    phrases_path: Path,
+    output_dir: Path,
+    volume: float,
+    workers: int,
+    create_subtitles: bool,
+    limit: int,
+) -> None:
+    movie_extension = movie_path.suffix
+    phrases_extension = phrases_path.suffix
 
-    with open(phrases_path) as f:
-        phrases = json.load(f)
+    if phrases_extension not in PHRASES_FILE_PARSERS_MAPPING:
+        raise ValueError(f"Unsupported phrases file extension: {phrases_extension}")
+
+    phrases_parser = PHRASES_FILE_PARSERS_MAPPING[phrases_extension]
+    phrases = phrases_parser(phrases_path)[:limit]
 
     base_cmd = f"ffmpeg -i {movie_path}"
-    output_str_groups = []
 
     for phrases_group in batched(phrases, workers):
-        output_group = []
+        output_str_groups = []
+
         for phrase in phrases_group:
-            start = parse_duration(phrase["start_in_movie"])
-            end = parse_duration(phrase["end_in_movie"])
-            volume_filter = f'-filter:a "volume={volume}"'
-            outputh_path = f"{output_dir}/{phrase['id']}.{movie_extension}"
+            start_arg = f"-ss {phrase.start}"
+            end_arg = f"-to {phrase.end}"
+            volume_arg = f'-filter:a "volume={volume}"'
+            output_path_arg = output_dir.joinpath(f"{phrase.output_name}.{movie_extension}")
 
-            output_group.append(f" -ss {start} -to {end} {volume_filter} {outputh_path}")
+            output_str_groups.append(f"{start_arg} {end_arg} {volume_arg} {output_path_arg}")
 
-        output_group_str = "".join(output_group)
-        output_str_groups.append(output_group_str)
-
-    for output_str in output_str_groups:
+        output_str = " ".join(output_str_groups)
         cmd = f"{base_cmd} {output_str} -y"
-
         subprocess.run(cmd, shell=True, check=True)  # noqa: S602
+
+    if create_subtitles:
+        handle_create_subtitles(phrases, output_dir)
 
 
 if __name__ == "__main__":
